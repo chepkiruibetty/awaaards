92987c465f9cb759cd5c18dfaff8a3471b33b3bd
chepkiruibetty <chepkiruibetty64@gmail.com>
2020-03-14 22:56:33 +0300

    register the classes in the admin

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Post, Profile
+admin.site.site_header='Betty Developer'
 
 # Register your models here.
+admin.site.register(Post)
+admin.site.register(Profile)
+
