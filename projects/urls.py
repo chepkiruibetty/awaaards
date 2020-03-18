83edf1ab5e7a61245126b994a583cdee4c4ebd60
chepkiruibetty <chepkiruibetty64@gmail.com>
2020-03-18 07:57:52 +0300

    deleted the urls plus static in the urls file

--- projects/urls.py
+++ projects/urls.py
@@ -24,4 +24,4 @@ path('search/', views.search_results, name='search_results'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-    urlpatterns += staticfiles_urlpatterns()
+    
